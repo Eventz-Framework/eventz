c0191b28c6df36051c1aa69731972d4166ed143f
Dan Ballance <work@danballance.uk>
2020-11-19 21:04:29 +0000

    Add Packet type to describe the packets that transport Events on the wire

---
+++ eventz/packets.py
@@ -0,0 +1,28 @@
+from typing import Tuple, Optional, Union, Dict, Iterable
+
+from eventz.messages import Event
+from eventz.value_object import ValueObject
+
+Payload = Union[Dict, Event, Tuple[str]]
+
+
+class Packet(ValueObject):
+    def __init__(
+        self,
+        subscribers: Iterable[str],
+        message_type: str,
+        route: str,
+        msgid: str,
+        dialog: str,
+        seq: int,
+        options: Optional[Iterable[str]] = None,
+        payload: Optional[Payload] = None,
+    ):
+        self.subscribers: Tuple[str] = tuple(subscribers)
+        self.message_type: str = message_type
+        self.route: str = route
+        self.msgid: str = msgid
+        self.dialog: str = dialog
+        self.seq: int = seq
+        self.options: Tuple[str] = tuple(options or [])
+        self.payload: Optional[Payload] = payload
