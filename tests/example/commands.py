b8705dc5f92f03132b516960d2b7b073d5890ce0
Dan Ballance <work@danballance.uk>
2021-05-08 07:30:08 +0100

    Fix Example commands that didn't allow msgid and timestamp to be set.

--- tests/example/commands.py
+++ tests/example/commands.py
@@ -1,11 +1,20 @@
+from datetime import datetime
+
 from eventz.messages import Command
 
 
 class CreateExample(Command):
     __version__: int = 1
 
-    def __init__(self, aggregate_id: str, param_one: int, param_two: str):
-        super().__init__(aggregate_id)
+    def __init__(
+        self,
+        aggregate_id: str,
+        param_one: int,
+        param_two: str,
+        __msgid__: str = None,
+        __timestamp__: datetime = None,
+    ):
+        super().__init__(aggregate_id, __msgid__, __timestamp__)
         self.param_one: int = param_one
         self.param_two: str = param_two
 
@@ -13,7 +22,14 @@ class CreateExample(Command):
 class UpdateExample(Command):
     __version__: int = 1
 
-    def __init__(self, aggregate_id: str, param_one: int, param_two: str):
-        super().__init__(aggregate_id)
+    def __init__(
+        self,
+        aggregate_id: str,
+        param_one: int,
+        param_two: str,
+        __msgid__: str = None,
+        __timestamp__: datetime = None,
+    ):
+        super().__init__(aggregate_id, __msgid__, __timestamp__)
         self.param_one: int = param_one
         self.param_two: str = param_two
